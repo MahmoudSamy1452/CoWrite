8ebc9b95eefc61e29263a7002fae356a433c5f50
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2024-03-18 23:58:31 +0200

    Spring boot starter code

---
+++ Backend/src/main/java/com/example/CoWrite/CoWriteApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoWrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoWriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoWriteApplication.class, args);
+	}
+
+}
