ffbc432fe4e90bf89fc28ac5e60c854d0b873fe4
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-04-10 09:34:10 +0200

    Implemented User Login

--- Backend/src/main/java/com/example/CoWrite/Controllers/AuthController.java
+++ Backend/src/main/java/com/example/CoWrite/Controllers/AuthController.java
@@ -1,13 +1,14 @@
 package com.example.CoWrite.Controllers;
 
 import com.example.CoWrite.Models.User;
-import com.example.CoWrite.Repositories.UserRepository;
 import com.example.CoWrite.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,5 +38,12 @@ public class AuthController {
             System.out.println("Error creating user" + e);
             return new ResponseEntity<>("Failed to create user", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User logged in successfully", HttpStatus.OK);
     }
 }
--- Backend/src/main/java/com/example/CoWrite/Services/UserService.java
+++ Backend/src/main/java/com/example/CoWrite/Services/UserService.java
@@ -29,6 +29,9 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
     }
 
-
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.singleton(new SimpleGrantedAuthority("user")));
     }
 }
