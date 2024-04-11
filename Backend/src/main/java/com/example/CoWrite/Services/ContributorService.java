3d54aa0e4ce98dfc5a23c8d8fa1cb427f879bc94
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-04-11 02:45:44 +0200

    Implemented Share Document Endpoint

--- Backend/src/main/java/com/example/CoWrite/Services/ContributorService.java
+++ Backend/src/main/java/com/example/CoWrite/Services/ContributorService.java
@@ -1,7 +1,11 @@
 package com.example.CoWrite.Services;
 
+import com.example.CoWrite.Exceptions.CouldNotDeleteException;
+import com.example.CoWrite.Exceptions.ResourceNotFoundException;
+import com.example.CoWrite.Models.Contributor;
 import com.example.CoWrite.Repositories.ContributorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,4 +18,20 @@ public class ContributorService {
         this.contributorRepository = contributorRepository;
     }
 
+    public Contributor shareDocument(Contributor contributor) throws ResourceNotFoundException, CouldNotDeleteException {
+//        Check if the user is already a contributor to the document
+        Contributor existingContributor = contributorRepository.findByUsernameAndDocument(contributor.getUsername(), contributor.getDocument());
+        if (existingContributor != null && existingContributor.getRole() == contributor.getRole()) {
+            throw new CouldNotDeleteException("User is already a contributor to the document");
+        }
+        try {
+            if (existingContributor != null){
+                existingContributor.setRole(contributor.getRole());
+                return contributorRepository.save(existingContributor);
+            }
+            return contributorRepository.save(contributor);
+        } catch (DataAccessException e) {
+            throw new ResourceNotFoundException(e.getMessage());
+        }
+    }
 }
