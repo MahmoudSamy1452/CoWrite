48fa926b002e64e91cd0cb4858452713ea00edd3
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-04-12 04:10:55 +0200

    Implemented Contributor methods to check authorization

--- Backend/src/main/java/com/example/CoWrite/Repositories/ContributorRepository.java
+++ Backend/src/main/java/com/example/CoWrite/Repositories/ContributorRepository.java
@@ -4,9 +4,13 @@ import com.example.CoWrite.Models.Contributor;
 import com.example.CoWrite.Models.Document;
 import com.example.CoWrite.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContributorRepository extends JpaRepository<Contributor, Long> {
     Contributor findByUsernameAndDocument(User username, Document document);
+
+    @Query("SELECT new com.example.CoWrite.DTOs.ContributorDTO(c.id, c.username, c.document, c.role) FROM Contributor c WHERE c.username.username = ?1 AND c.document.id = ?2")
+    com.example.CoWrite.DTOs.ContributorDTO findByUsernameAndDocument(String username, Long documentId);
 }
--- Backend/src/main/java/com/example/CoWrite/Services/ContributorService.java
+++ Backend/src/main/java/com/example/CoWrite/Services/ContributorService.java
@@ -1,8 +1,11 @@
 package com.example.CoWrite.Services;
 
-import com.example.CoWrite.Exceptions.CouldNotDeleteException;
+import com.example.CoWrite.DTOs.ContributorDTO;
+import com.example.CoWrite.Exceptions.BadRequestException;
 import com.example.CoWrite.Exceptions.ResourceNotFoundException;
 import com.example.CoWrite.Models.Contributor;
+import com.example.CoWrite.Models.Document;
+import com.example.CoWrite.Models.User;
 import com.example.CoWrite.Repositories.ContributorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -13,17 +16,22 @@ public class ContributorService {
 
     private final ContributorRepository contributorRepository;
 
+    private final UserService userService;
+
     @Autowired
-    public ContributorService(ContributorRepository contributorRepository) {
+    public ContributorService(ContributorRepository contributorRepository, UserService userService) {
         this.contributorRepository = contributorRepository;
+        this.userService = userService;
     }
 
-    public Contributor shareDocument(Contributor contributor) throws ResourceNotFoundException, CouldNotDeleteException {
+    public Contributor shareDocument(Contributor contributor) throws ResourceNotFoundException, BadRequestException {
 //        Check if the user is already a contributor to the document
         Contributor existingContributor = contributorRepository.findByUsernameAndDocument(contributor.getUsername(), contributor.getDocument());
         if (existingContributor != null && existingContributor.getRole() == contributor.getRole()) {
-            throw new CouldNotDeleteException("User is already a contributor to the document");
+            throw new BadRequestException("User is already a " + (contributor.getRole() == 'v' ? "viewer" : "editor") + " to the document");
         }
+        if (contributor.getRole() != 'e' && contributor.getRole() != 'v')
+            throw new BadRequestException("Can only share with an editor or a viewer");
         try {
             if (existingContributor != null){
                 existingContributor.setRole(contributor.getRole());
@@ -33,5 +41,29 @@ public class ContributorService {
         } catch (DataAccessException e) {
             throw new ResourceNotFoundException(e.getMessage());
         }
+    }
+
+    public void setOwner(String username, Document document) throws ResourceNotFoundException {
+        Contributor contributor = new Contributor();
+        contributor.setDocument(document);
+        User user = userService.getUser(username);
+        contributor.setUsername(user);
+        contributor.setRole('o');
+        contributorRepository.save(contributor);
+    }
+
+    public boolean isOwner(String username, Long documentId){
+        ContributorDTO existingContributor = contributorRepository.findByUsernameAndDocument(username, documentId);
+        return existingContributor != null && existingContributor.getRole() == 'o';
+    }
+
+    public boolean isViewer(String username, Long documentId){
+        ContributorDTO existingContributor = contributorRepository.findByUsernameAndDocument(username, documentId);
+        return existingContributor != null && existingContributor.getRole() == 'v';
+    }
+
+    public boolean isEditor(String username, Long documentId){
+        ContributorDTO existingContributor = contributorRepository.findByUsernameAndDocument(username, documentId);
+        return existingContributor != null && existingContributor.getRole() == 'e';
     }
 }
