7670550d3509fbd3e6948fbba652e8156bcaff51
islam amr <islam.mohamed02@eng-st.cu.edu.eg>
2024-04-13 15:06:52 +0200

    Integrated signup logic

--- Frontend/src/components/SignUpForm.jsx
+++ Frontend/src/components/SignUpForm.jsx
@@ -1,6 +1,62 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { VITE_BACKEND_URL } from "../../config.js";
+
 const SignUpForm = () => {
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+  const { dispatch } = useAuthContext();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if (e.target.username.value === "" || e.target.password.value === "" || e.target.confirmPassword.value === "") {
+      setError("Please fill out all fields");
+      return;
+    }
+
+    const regex = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
+
+    if (!regex.test(e.target.password.value)) {
+      setError("Password must be between 8-16 characters and contain at least one number, one uppercase letter, one lowercase letter, and one special character");
+      return;
+    }
+    
+    if (e.target.password.value !== e.target.confirmPassword.value) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    axios.defaults.withCredentials = true;
+
+    axios.post(`${VITE_BACKEND_URL}/auth/register`, {
+      username: e.target.username.value,
+      password: e.target.password.value,
+    })
+    .then((res) => {
+      if (res.status === 201) {
+        dispatch({ type: 'LOGIN', user: e.target.username.value, token: res.accessToken });
+        navigate('/home');
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      console.log("Cannot signup");
+      setError("Username already exists");
+    });
+  }
+
   return (
-    <form className="m-auto border border-white-300 rounded" action="">
+    <form className="m-auto border border-white-300 rounded max-w-96" onSubmit={handleSubmit}>
+      {error && (
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative">
+            <strong className="font-bold">Error!</strong>
+            <br />
+            <span className="block sm:inline"> {error}</span>
+          </div>
+      )}
       <p className="text-blue-500 text-3xl mt-5">Sign Up!</p>
       <div className="relative mt-6">
         <input
